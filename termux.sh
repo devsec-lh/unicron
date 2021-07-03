dd72a850dc4b54917942e2614504f0a5be273211
crybaby <crybaby@psycho.maldite.mil.gov>
2021-07-03 16:42:32 -0400

    first commit

---
+++ termux.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+pkg update -y && pkg full-upgrade -y; pkg install wget -y; pkg install git -y;
+pkg install npm -y; pkg install nodejs -y;  pkg install termux** -y;
--- utils.sh
+++ utils.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-whatthefuck={ pwd && uname -a && whoiam;
-echo $whatthefuck
+whatthefuck={ pwd && uname -a;
+echo $whatthefuck;
+apt-get update -y && apt-get full-upgrade -y;
